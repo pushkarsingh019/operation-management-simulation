b65d9a72030c226279dfd4a16238b727505630fe
Pushkar Singh <stoazest@gmail.com>
2023-04-14 10:27:02 +0530

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <section>
+            <h1>Operation Manegent Simulation</h1>
+            <Link href="/test">Test Route</Link>
+        </section>
+    );
+}
