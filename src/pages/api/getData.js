b7e3eed1f4a8c5e1ff95995eabdabad9595c49e5
Pushkar Singh <stoazest@gmail.com>
2023-04-16 20:09:32 +0530

    chart basic working

--- src/pages/api/getData.js
+++ src/pages/api/getData.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
     try {
       connectDatabase();
       const { roundNumber, roomName } = req.body;
-      console.log(roundNumber);
       const choices = await Choice.find({ roundNumber: roundNumber, roomName : roomName });
       let data = choices.map(choice => ({username : choice.username, netValue : choice.netValue}));
       res.send(data)
