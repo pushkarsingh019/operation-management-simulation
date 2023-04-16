30ce30afaa9c2c2b72529137e1cac9e1c6b40a61
Pushkar Singh <stoazest@gmail.com>
2023-04-16 21:29:44 +0530

    roundThree working

--- src/pages/[gameId]/roundThree.jsx
+++ src/pages/[gameId]/roundThree.jsx
@@ -1,8 +1,136 @@
+import { useState } from "react";
+import { useContext } from "react";
+import { CartContext } from "@/utils/store";
+
+import {
+    getProductionTime,
+    getCapacityUtilisation,
+    getLabourCost,
+    getCostPerUnit,
+    getAverageWipCost,
+} from "../../utils/roundThree";
+
+import { getNetValue } from "../../utils/gameFunctions";
+import { useRouter } from "next/router";
+import axios from "axios";
+
 export default function Round3() {
+    const { roomName, username } = useContext(CartContext);
+    const router = useRouter();
+    const [lotSize, setLotSize] = useState(55500);
+    const [productionTime, setProductionTime] = useState(
+        getProductionTime(lotSize)
+    );
+    const [capacityUtlisation, setCapacityUtilisation] = useState(
+        getCapacityUtilisation(lotSize)
+    );
+    const [labourCost, setLabourCost] = useState(getLabourCost(lotSize));
+    const [costPerUnit, setCostPerUnit] = useState(getCostPerUnit(lotSize));
+    const [wipCost, setWipCost] = useState(getAverageWipCost(lotSize));
+
+    const lotSizeHandler = (event) => {
+        setLotSize(event.target.value);
+        setProductionTime(getProductionTime(lotSize));
+        setCapacityUtilisation(getCapacityUtilisation(lotSize));
+        setLabourCost(getLabourCost(lotSize));
+        setCostPerUnit(getCostPerUnit(lotSize));
+        setWipCost(getAverageWipCost(lotSize));
+    };
+
+    const onFormSubmit = async (event) => {
+        event.preventDefault();
+        const netValue = getNetValue(lotSize);
+        let { data } = await axios.post(`/api/choice`, {
+            username,
+            roomName,
+            netValue,
+            roundNumber: 3,
+        });
+        router.push(`/chart/3`);
+    };
+
     return (
         <section>
-            <h2>Round Three</h2>
-            <p>this is the round three</p>
+            <h3>Round Three</h3>
+            <p>
+                {" "}
+                The players in this round will be asked to free up the working
+                capital or reduce the cost. That means the players must select
+                the optimal amount of Lot size to determine the output. The
+                optimal production will be considered against the value of lot
+                size 55500.
+            </p>
+            <br />
+            <br />
+            <form onSubmit={(event) => onFormSubmit(event)}>
+                <label>Lot Size</label>
+                <input
+                    type="range"
+                    placeholder="lot size"
+                    min={50000}
+                    max={60000}
+                    value={lotSize}
+                    onChange={(event) => lotSizeHandler(event)}
+                />
+                <small>
+                    {"   "}
+                    {lotSize}
+                </small>
+                <br />
+                <br />
+                <label>Total Production Time</label>
+                <br />
+                <input
+                    type="Number"
+                    placeholder="Total Production Time"
+                    value={productionTime}
+                    disabled
+                />
+                <br />
+                <br />
+                <label>Capacity Utilisation</label>
+                <br />
+                <input
+                    type="Number"
+                    placeholder="Capacity Utilisation"
+                    value={capacityUtlisation}
+                    disabled
+                />
+                <br />
+                <br />
+                <label>Labour Cost</label>
+                <br />
+                <input
+                    type="Number"
+                    placeholder="Labour Cost"
+                    value={labourCost}
+                    disabled
+                />
+                <br />
+                <br />
+                <label>Cost Per Unit</label>
+                <br />
+                <input
+                    type="Number"
+                    placeholder="Cost per unit"
+                    value={costPerUnit}
+                    disabled
+                />
+                <br />
+                <br />
+                <label>Average Work In Progress Cost</label>
+                <br />
+                <input
+                    type="Number"
+                    placeholder="Average wip cost"
+                    value={wipCost}
+                    disabled
+                />
+                <br />
+                <br />
+                <br />
+                <button type="submit">Next</button>
+            </form>
         </section>
     );
 }
---
+++ src/utils/roundThree.js
@@ -0,0 +1,34 @@
+export const caseOne = {
+    volume : 6500000,
+    setupTimePerLot : 60,
+    totalAvailableTime : 8760,
+    runTimeSecondsPerUnit : 0.15,
+    averageLabourRate : 120,
+    employeesPerShift : 25,
+    totalMaterialCost : 120000000,
+    scrapMaterialPerSetup : 400,
+    wipAvailable : 10,
+};
+
+export const getProductionTime = (lotSize) => {
+    const totalSetupTime = (caseOne.volume / lotSize) * 60;
+    const totalRunTime = (caseOne.runTimeSecondsPerUnit * caseOne.volume) / 3600;
+    const totalProductionTime = (totalSetupTime + totalRunTime)
+    return Math.round(totalProductionTime * 10) / 10;
+};
+
+export const getLabourCost = (lotSize) => Number(Math.round(caseOne.averageLabourRate * caseOne.employeesPerShift * getProductionTime(lotSize) * 10) / 10);
+
+export const getScrapCost = (lotSize) => (caseOne.volume / lotSize) * caseOne.scrapMaterialPerSetup;
+
+export const getProductionCost = (lotSize) => caseOne.totalMaterialCost + getLabourCost(lotSize) + getScrapCost(lotSize);
+
+export const wipNumberOfUnits = (lotSize) => caseOne.wipAvailable * lotSize;
+
+export const getCostPerUnit = (lotSize) => Math.round((getProductionCost(lotSize) / caseOne.volume) * 10) / 10;
+
+export const getCapacityUtilisation = lotSize => {
+    return Math.round(((getProductionTime(lotSize) / caseOne.totalAvailableTime) * 100 ) * 10) / 10
+};
+
+export const getAverageWipCost = lotSize => Math.round((wipNumberOfUnits(lotSize) / getCostPerUnit(lotSize)) * 10) / 10
